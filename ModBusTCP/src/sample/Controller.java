27a2406547c5d2a1af42df34ffe4a3bbaa371f67
Aman Ullah <amanullah.en@gmail.com>
2017-12-04 09:52:21 +0600

    adding resource for next step

--- ModBusTCP/src/sample/Controller.java
+++ ModBusTCP/src/sample/Controller.java
@@ -35,13 +35,16 @@ public class Controller {
         dtd.add(0);
         dtd.add(33);
         //System.out.println("Send funciton working!");
-        List<Byte> dt= modbus.writeMultipleHoldingRegisters(50045, dtd);
+        List<Byte> dt= modbus.readInputRegisters(1, 2);
         System.out.println(dt.size());
-        System.out.println(dt.get(7));
+        //System.out.println(dt.get(7));
         for (int i=0; i < dt.size(); i++) {
             System.out.print( String.format("%02X - ", dt.get(i)));
         }
+        //float rv = Float.intBitsToFloat((int) ((dt.get(9)<<24) | (dt.get(10)<<16) | (dt.get(11)<<8) | dt.get(12)));
+        float rv = Float.intBitsToFloat((int) Long.parseLong(String.format("%02X%02X%02X%02X", dt.get(9),dt.get(10),dt.get(11),dt.get(12)),16));
         System.out.println("");
+        System.out.println(rv);
 
     }
 }
